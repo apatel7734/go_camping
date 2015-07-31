177f92b51fcc478f793515683b79ca48f19bd934
Ashish Patel <a.patel7734@gmail.com>
2015-07-31 13:30:54 -0700

    add family UIViewcontroller

---
+++ Expenses/AddFamilyUIViewController.swift
@@ -0,0 +1,24 @@
+//
+//  AddFamilyUIViewController.swift
+//  Expenses
+//
+//  Created by Ashish Patel on 7/31/15.
+//  Copyright (c) 2015 Ashish Patel. All rights reserved.
+//
+
+import UIKit
+
+class AddFamilyUIViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
