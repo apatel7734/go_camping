632385e014a748b5e820a0f846cd232a7121c086
Ashish Patel <a.patel@capitalone.com>
2015-12-21 16:14:49 -0800

    fix label lengh

--- Expenses/ErrorView.swift
+++ Expenses/ErrorView.swift
@@ -12,9 +12,7 @@ class ErrorView {
     
     static let sharedView = ErrorView()
     private let errorLabel: UILabel!
-    private let viewHeight: CGFloat = 50.0
-    private let affineTranslate = CGAffineTransformMakeTranslation(0, 50)
-    private var errorViewOriginalBounds: CGRect!
+    private let viewHeight: CGFloat = 70.0
     
     private init(){
         let errorView = NSBundle.mainBundle().loadNibNamed("ErrorView", owner: nil, options: nil)
@@ -25,25 +23,24 @@ class ErrorView {
         errorLabel.text = message
         setErrorLabelFrame(view)
         view.addSubview(errorLabel)
-        resetTransform()
         animateErrorView()
     }
-
+    
+    func hideErrorView(){
+        UIView.animateWithDuration(1.0) { () -> Void in
+            self.errorLabel.frame.origin.y = -self.viewHeight
+        }
+    }
+    
     private func setErrorLabelFrame(view: UIView){
-        errorViewOriginalBounds = view.bounds
-        errorLabel.frame = view.bounds
+        errorLabel.frame = view.frame
         errorLabel.frame.size.height = viewHeight
+        errorLabel.frame.origin.y = -viewHeight
     }
     
     private func animateErrorView(){
         UIView.animateWithDuration(1.0) { () -> Void in
-            let affineTranslate = CGAffineTransformMakeTranslation(self.errorViewOriginalBounds.origin.x,self.errorViewOriginalBounds.origin.y)
-            self.errorLabel.transform = affineTranslate
+            self.errorLabel.frame.origin.y = 0.0
         }
-    }
-    
-    private func resetTransform(){
-        let affineTranslate = CGAffineTransformMakeTranslation(self.errorViewOriginalBounds.origin.x,self.errorViewOriginalBounds.origin.y - viewHeight)
-        self.errorLabel.transform = affineTranslate
     }
 }
