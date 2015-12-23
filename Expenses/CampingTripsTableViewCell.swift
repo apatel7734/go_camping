5ecbe94c07250a030020e618feae200650496d23
Ashish Patel <a.patel@capitalone.com>
2015-12-23 15:02:02 -0800

    add camping trip viewcontroller + tableview cell UI + outlets

---
+++ Expenses/CampingTripsTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  campingTripsTableViewCell.swift
+//  Expenses
+//
+//  Created by Patel, Ashish on 12/23/15.
+//  Copyright © 2015 Ashish Patel. All rights reserved.
+//
+
+import UIKit
+
+class CampingTripsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var campingTripTitleLabel: UILabel!
+    
+    @IBOutlet weak var camptingTripFromDateLabel: UILabel!
+    
+    @IBOutlet weak var campingTripToDateLabel: UILabel!
+    
+    @IBOutlet weak var campingTripAddressLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Expenses/ListCampingTripsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ListCampingTripsViewController.swift
+//  Expenses
+//
+//  Created by Patel, Ashish on 12/23/15.
+//  Copyright © 2015 Ashish Patel. All rights reserved.
+//
+
+import UIKit
+
+class ListCampingTripsViewController: UIViewController {
+    
+    @IBOutlet weak var campingTripsTableView: UITableView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
