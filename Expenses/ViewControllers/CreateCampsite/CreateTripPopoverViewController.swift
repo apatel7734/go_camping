bc74bb15fa830fc023a59f7535ea5510af29cc9e
emmasuzuki <emma11suzuki@gmail.com>
2016-02-29 01:16:08 -0800

    Add datepicker

--- Expenses/ViewControllers/CreateCampsite/CreateTripPopoverViewController.swift
+++ Expenses/ViewControllers/CreateCampsite/CreateTripPopoverViewController.swift
@@ -9,8 +9,31 @@
 import UIKit
 
 class CreateTripPopoverViewController: UIViewController {
-
+    
+    @IBOutlet weak var titleField: UITextField!
+    @IBOutlet weak var startDateField: UITextField!
+    @IBOutlet weak var endDateField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let datePicker = UIDatePicker()
+        datePicker.datePickerMode = .Date
+        startDateField.inputView = datePicker
+        endDateField.inputView = datePicker
+        
+        let accessoryView = UIToolbar(frame: CGRectMake(0, 0, view.bounds.width, 50))
+        accessoryView.items = [
+            UIBarButtonItem(barButtonSystemItem: .FlexibleSpace, target: nil, action: nil),
+            UIBarButtonItem(title: "Done", style: .Done, target: self, action: "accessoryDoneButtonTapped:")
+        ]
+        accessoryView.sizeToFit()
+        
+        startDateField.inputAccessoryView = accessoryView
+        endDateField.inputAccessoryView = accessoryView
+    }
+    
+    func accessoryDoneButtonTapped(sender: UIView) {
+        print(sender)
     }
 }
