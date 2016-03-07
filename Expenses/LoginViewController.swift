81c8d6d5d05715d4cf1a3ddec72e5e9cf3111cee
Ashish Patel <a.patel@capitalone.com>
2016-03-06 22:14:14 -0800

    add login screen

---
+++ Expenses/LoginViewController.swift
@@ -0,0 +1,33 @@
+//
+//  LoginViewController.swift
+//  Expenses
+//
+//  Created by Patel, Ashish on 3/6/16.
+//  Copyright © 2016 Ashish Patel. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var phoneNumber: NoCaretTextField!
+    @IBOutlet weak var password: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func didTapOnLoginButton(sender: AnyObject) {
+    
+        print("Login Tapped.")
+    }
+    
+    @IBAction func didTapoMainView(sender: AnyObject) {
+        self.view.endEditing(true)
+    }
+    
+    
+    
+}
